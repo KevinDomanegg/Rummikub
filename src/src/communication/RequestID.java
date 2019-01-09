29bf40a6ba76e4a6f1124dc09a27cdd139b8274f
Johannes Messner <messnerjo@gmail.com>
2019-01-09 16:20:39 +0100

    Added Interfaces for the communication between the clients and the server.

--- src/src/communication/RequestID.java
+++ src/src/communication/RequestID.java
@@ -5,6 +5,6 @@ public enum RequestID {
   /**
    * Identifier for classes implementing the Request-Interface.
    * Every implementation of Request should have an RequestID associated with it.
-   */
+   */uidhuf
 
 }
