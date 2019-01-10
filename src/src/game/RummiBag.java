3d4786fdab1fcfc4ddda907aa3c039951dea74f9
Cedrik Harrich <harrich@cip.ifi.lmu.de>
2019-01-10 10:25:49 +0100

    Bag is done.

--- src/src/game/RummiBag.java
+++ src/src/game/RummiBag.java
@@ -1,10 +1,13 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RummiBag {
   private ArrayList<Stone> stones = new ArrayList<>();
+  private Random randomGenerator;
   private final static int STONESOFEACHCOLOR = 13;
+
 
   //The bag is initially filled with 13 Numbers of each
   //color and two Jokers.
@@ -18,10 +21,22 @@ public class RummiBag {
     }
     stones.add(new Stone(Stone.Color.JOKER, 0));
     stones.add(new Stone(Stone.Color.JOKER, 0));
+
+    randomGenerator = new Random();
   }
 
+  //Returns a random Stone.
   public Stone removeStone(){
-    return null;
+    int index = randomGenerator.nextInt(stones.size());
+    Stone stone = stones.get(index);
+    return stone;
   }
 
+  public int size() {
+    return stones.size();
+  }
+
+  public void addStones(ArrayList<Stone> givenStones){
+    stones.addAll(givenStones);
+  }
 }
