dfc3b421590b026230166d5c4e269397ae49939b
kleinhans <till.kleinhans@campus.lmu.de>
2019-01-10 14:18:42 +0100

    Draw button adds Rectangle objects to hand

--- src/src/View/GameController.java
+++ src/src/View/GameController.java
@@ -1,18 +1,50 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class GameController {
-  @FXML
-  Button drawButton;
+  int handCount = 0;
 
-  @FXML
-  Text timer;
+  @FXML Button drawButton;
+  @FXML Text timer;
+  @FXML GridPane hand;
+  @FXML StackPane opponentMid;
 
-  public GameController() {}
+  String name = "Hannah";
+
+  public GameController() {
+  }
 
   @FXML
   public void drawStone() {
-    timer.setText("Hello!");
+    // Server request: Get stone from bag
+    Rectangle rectangle = new Rectangle(30,50);
+    rectangle.setId("handStone" + handCount);
+    handCount++;
+    hand.add(rectangle, handCount,0);
   }
+
+  public void nameChange() {
+    //TODO: Doesnt work
+    Node opMidName = opponentMid.getChildren().get(0);
+    if (opMidName instanceof Text) {
+      ((Text) opMidName).setText(name);
+    }
+  }
+
+
+  //TODO: Remove this Stone class
+  class Stone extends VBox {
+    Stone() {
+
+    }
+
+  }
+
 }
