b85276d9e7636b6c3546b6b6f7eb93095f44b60a
kleinhans <till.kleinhans@campus.lmu.de>
2019-01-14 13:09:51 +0100

    Cleaned up commit for today's deliverable

--- src/src/View/GameController.java
+++ src/src/View/GameController.java
@@ -1,19 +1,22 @@
 package View;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.io.File;
 import java.io.IOException;
 
 public class GameController {
@@ -24,7 +27,6 @@ public class GameController {
   @FXML GridPane table;
   @FXML GridPane handGrid;
   @FXML Pane opponentMid;
-  @FXML Text testText;
 
   String name = "Player";
 
@@ -35,49 +37,22 @@ public class GameController {
   public void initialize() {
     constructGrid(table, 24, 8);
     constructGrid(handGrid, 20, 2);
-    /*
-    TESTING
-
-    testText.setOnDragDetected(event -> {
-      Dragboard dragBoard = testText.startDragAndDrop(TransferMode.ANY);
-      ClipboardContent content = new ClipboardContent();
-      content.putString(testText.getText());
-      dragBoard.setContent(content);
-    });
-    */
   }
 
   @FXML
   public void drawStone() throws IOException {
     // Server request: Get stone from bag
     VBox cell = FXMLLoader.load(getClass().getResource("Stone.fxml"));
-
-    /* TODO: Add eventHandler for drag and drop
-    cell.setOnDragDetected(event -> {
-      Dragboard dragBoard = cell.startDragAndDrop(TransferMode.ANY);
-      ClipboardContent content = new ClipboardContent();
-      content.put
-      dragBoard.setContent(content);
-    });
-    */
-
     handGrid.add(cell, handCount, 0);
     handCount++;
   }
-
-  public void nameChange() {
-    Node opMidName = opponentMid.getChildren().get(0);
-    if (opMidName instanceof Text) {
-      ((Text) opMidName).setText(name);
-    }
-  }
-
 
   @FXML
   void constructGrid(GridPane grid, int columns, int rows) {
     for(int x = 0; x < columns; x++) {
       for(int y = 0; y < rows; y++) {
         StackPane cell = new StackPane();
+        //setupDragAndDrop(cell);
         cell.setPrefWidth(1024.0/columns); //TODO: Configure for hand, too
         cell.setPrefHeight(768.0/rows);
         cell.getStyleClass().add("cell");
@@ -85,5 +60,38 @@ public class GameController {
       }
     }
   }
+
+  /*
+  void setupDragAndDrop(StackPane cell) {
+
+    // Start drag here
+    cell.setOnDragDetected(event -> {
+      Dragboard dragBoard = cell.startDragAndDrop(TransferMode.ANY);
+      ClipboardContent content = new ClipboardContent();
+      // TODO: Put stone to clipboard
+      if (!cell.getChildren().isEmpty()) {
+        content.put(DataFormat.FILES, cell.getChildren());
+        dragBoard.setContent(content);
+      }
+      event.consume();
+    });
+
+    // Accept drop here
+    cell.setOnDragOver(event -> {
+      event.acceptTransferModes(TransferMode.ANY);
+      event.consume();
+    });
+
+    // Drop here
+    cell.setOnDragDropped(event -> {
+      Dragboard dragboard = event.getDragboard();
+      if (dragboard.hasFiles()) {
+        cell.getChildren().addAll(dragboard.getFiles());
+      }
+      //TODO: Put dragboard content as node children
+      event.consume();
+    });
+  }
+  */
 
 }
