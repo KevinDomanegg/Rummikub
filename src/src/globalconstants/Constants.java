0b591daa4a77921915d5ecf3a7cace1c133a2ba2
Cedrik Harrich <harrich@cip.ifi.lmu.de>
2019-02-03 09:38:48 +0100

    Bugfix: Added Global Constants

---
+++ src/src/globalconstants/Constants.java
@@ -0,0 +1,17 @@
+package globalconstants;
+
+public class Constants {
+  public static final int MIN_PLAYERS = 2;
+  public static final int MAX_PLAYERS = 4;
+
+  public static final int FIRST_STONES = 14;
+  public static final int MIN_FIRST_MOVE_POINTS = 30;
+
+  public static final int TABLE_HEIGHT = 8;
+  public static final int TABLE_WIDTH = 26;
+
+  public static final int HAND_HEIGHT = 3;
+  public static final int HAND_WIDTH = 18;
+
+  public static final int JOKER_POINTS = 10;
+}
